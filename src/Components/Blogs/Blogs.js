30597fd28f3b20c15974d58ccfb358e0e7410428
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-04-04 12:45:27 +0600

    blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            <h2>What is Context API ?</h2>
-            <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
-            <h2></h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>What is Context API ?</h2>
+      <p>
+        The Context API is a React structure that enables you to exchange unique
+        details and assists in solving prop-drilling from all levels of your
+        application.
+      </p>
+      <h2> What is Semantic tag?</h2>
+      <p>
+        Semantic HTML elements are those that clearly describe their meaning in
+        a human- and machine-readable way. Elements such as 'header' , footer
+        and article are all considered semantic because they accurately describe
+        the purpose of the element and the type of content that is inside them
+      </p>
+    </div>
+  );
 };
 
 export default Blogs;
